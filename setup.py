eab60f2e10f72abea7fad727cd11aaf61808463b
sonofeft <ctatsourceforge@gmail.com>
2015-09-04 20:53:50 -0700

    added some mock logic to setup.py

--- setup.py
+++ setup.py
@@ -31,9 +31,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# If on the ReadTheDocs build site, simply mock the dependencies
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
-    requires = ['mock']
+    if sys.version_info < (3,3):
+        requires = ['mock']   # for python2 and python < 3.3
+    else:
+        requires = []  # for >= python3.3
+    
 else:
     # Place install_requires into the text file "requirements.txt"
     with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f2:
